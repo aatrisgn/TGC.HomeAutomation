4b1e02f9b19d9599bca84feb3e4637a250d0605f
Asger Thyregod <asger.thyregod@gmail.com>
2025-03-17 21:04:55 +0100

    Added required scopes on app registration

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,3 +87,18 @@ resource "azuread_application_redirect_uris" "example_spa" {
     "http://localhost:4200"
   ]
 }
+
+data "azuread_application_published_app_ids" "well_known" {}
+
+data "azuread_service_principal" "msgraph" {
+  client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+}
+
+resource "azuread_application_api_access" "example_msgraph" {
+  application_id = azuread_application_registration.web_auth_app_registration.id
+  api_client_id  = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+
+  scope_ids = [
+    data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.ReadWrite"],
+  ]
+}
