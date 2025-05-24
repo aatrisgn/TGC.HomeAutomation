922102f36b5483b9ce9296c19ea8cb9d23fed008
Asger Thyregod <asger.thyregod@gmail.com>
2025-05-24 20:50:56 +0200

    Ensured it being a set

--- terraform/runner_identity/main.tf
+++ terraform/runner_identity/main.tf
@@ -17,25 +17,25 @@ terraform {
 }
 
 resource "azuread_application_registration" "runner_app_registration" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   display_name = "tgc-homeautomation-${each.key}-${lower(var.environment)}"
 }
 
 resource "azuread_service_principal" "runner_enterprise_application" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   client_id = azuread_application_registration.runner_app_registration[each.key].client_id
 }
 
 resource "azuread_application_password" "runner_secret" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   application_id = azuread_application_registration.runner_app_registration[each.key].id
 }
 
 resource "azurerm_key_vault_secret" "runner_client_id" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   name         = "${each.key}ClientId"
   key_vault_id = data.azurerm_key_vault.shared_keyvault.id
@@ -43,7 +43,7 @@ resource "azurerm_key_vault_secret" "runner_client_id" {
 }
 
 resource "azurerm_key_vault_secret" "runner_object_id" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   name         = "${each.key}ObjectId"
   key_vault_id = data.azurerm_key_vault.shared_keyvault.id
@@ -51,7 +51,7 @@ resource "azurerm_key_vault_secret" "runner_object_id" {
 }
 
 resource "azurerm_key_vault_secret" "runner_secret" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   name         = "${each.key}Password"
   key_vault_id = data.azurerm_key_vault.shared_keyvault.id
@@ -59,7 +59,7 @@ resource "azurerm_key_vault_secret" "runner_secret" {
 }
 
 resource "azurerm_role_assignment" "table_storage_contributor" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   scope                = data.azurerm_storage_account.homeautomation_storage_account.id
   role_definition_name = "Storage Table Data Contributor"
@@ -67,10 +67,9 @@ resource "azurerm_role_assignment" "table_storage_contributor" {
 }
 
 resource "azurerm_role_assignment" "storage_account_reader" {
-  for_each = var.raspberries
+  for_each = toset(var.raspberries)
 
   scope                = data.azurerm_storage_account.homeautomation_storage_account.id
   role_definition_name = "Reader"
   principal_id         = azuread_service_principal.runner_enterprise_application[each.key].object_id
 }
-
