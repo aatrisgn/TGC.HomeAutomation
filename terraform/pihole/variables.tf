b10f0321a9c1a35892940ccdd681199f32ad80c2
Asger Thyregod <asger.thyregod@gmail.com>
2025-05-30 18:24:50 +0200

    Removed tenant id and added missing parameter

--- terraform/pihole/variables.tf
+++ terraform/pihole/variables.tf
@@ -7,8 +7,3 @@ variable "keyvault_name" {
   description = "Shared Homeautomation keyvault for credential storage"
   type        = string
 }
-
-variable "tenant_id" {
-  description = "Azure resource group to deploy resources into"
-  type        = string
-}
