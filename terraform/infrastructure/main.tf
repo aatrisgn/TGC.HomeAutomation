b102967db2370618e65d0ca98d3fa9f932137084
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-06-24 21:27:30 +0200

    Fixed name

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_application_insights" "application_insights" {
 
 resource "azurerm_key_vault_secret" "ai_connectionkey" {
   key_vault_id = azurerm_key_vault.shared_keyvault.id
-  name         = "application_insights_connection_string"
+  name         = "application-insights-connection-string"
   value        = azurerm_application_insights.application_insights.connection_string
 }
 
