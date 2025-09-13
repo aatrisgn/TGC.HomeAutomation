833ccbfc3b07c1f3ab26cd5ffcc0dbb9e9d819fc
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-13 16:16:45 +0200

    Added a test console app and trying to push to ACR

---
+++ src/TGC.HomeAutomation/TGC.HomeAutomation.TestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+Console.WriteLine("Starting application...");
+
+
+string keyVaultUrl = "https://tgckvhadev.vault.azure.net/";
+string secretName = "test-value";
+
+var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+
+Console.WriteLine($"Secret value: {secret.Value}");
