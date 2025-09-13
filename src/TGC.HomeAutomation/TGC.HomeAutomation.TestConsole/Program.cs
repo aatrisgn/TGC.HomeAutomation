da4c7312de22fa9a481b11e1558222bae86eb144
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-13 17:46:58 +0200

    Testing somethign more

--- src/TGC.HomeAutomation/TGC.HomeAutomation.TestConsole/Program.cs
+++ src/TGC.HomeAutomation/TGC.HomeAutomation.TestConsole/Program.cs
@@ -9,7 +9,12 @@ Console.WriteLine("Starting application...");
 string keyVaultUrl = "https://tgckvhadev.vault.azure.net/";
 string secretName = "test-value";
 
-var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+var options = new DefaultAzureCredentialOptions
+{
+	ManagedIdentityClientId = "19ec7ba5-8986-4d46-84a3-c27524ceb2b6"
+};
+
+var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential(options));
 
 KeyVaultSecret secret = await client.GetSecretAsync(secretName);
 
