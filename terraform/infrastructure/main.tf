e2c80015f1551c1bdb924dc96bc05f466082410a
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-13 20:08:51 +0200

    Adjusted redirect for new hosting

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -112,7 +112,9 @@ resource "azuread_application_redirect_uris" "example_spa" {
     "http://localhost:4300",
     "http://localhost:4200",
     "https://homeautomation.dev.tgcportal.com",
-    "https://homeautomation.dev.tgcportal.com/auth"
+    "https://homeautomation.dev.tgcportal.com/auth",
+    "https://app.homeautomation.dev.tgcportal.com",
+    "https://app.homeautomation.dev.tgcportal.com/auth"
   ]
 }
 
