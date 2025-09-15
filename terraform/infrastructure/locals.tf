d91859ed46e9d97eda35cf0f8162d801ecc1fd97
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-15 21:27:12 +0200

    Formatted

--- terraform/infrastructure/locals.tf
+++ terraform/infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  api_read_value = "api.read"
-  api_manage_value = "api.manage"
+  api_read_value    = "api.read"
+  api_manage_value  = "api.manage"
   api_signalr_value = "api.signalr"
 }
